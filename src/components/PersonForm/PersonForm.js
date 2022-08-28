eeca3121d02525a73fcf818ed819ae32d8858f09
Lawrence Dovin <lawrencedovin@gmail.com>
2022-08-28 14:16:46 -0400

    updated floating inputs and upload image styles

--- src/components/PersonForm/PersonForm.js
+++ src/components/PersonForm/PersonForm.js
@@ -4,38 +4,38 @@ import "./PersonForm.css";
 const PersonForm = () => {
     return (
         // <form>
-        //     <div class="input-group input-group-sm">
-        //         <span class="input-group-text">First and Last name</span>
-        //         <input type="text" aria-label="First name" class="form-control" />
-        //         <input type="text" aria-label="Last name" class="form-control" />
+        //     <div className="input-group input-group-sm">
+        //         <span className="input-group-text">First and Last name</span>
+        //         <input type="text" aria-label="First name" className="form-control" />
+        //         <input type="text" aria-label="Last name" className="form-control" />
         //     </div>
-        //     <div class="mb-3">
-        //         <label for="formFileSm" class="form-label">Upload Image</label>
-        //         <input class="form-control form-control-sm" id="formFileSm" type="file" />
+        //     <div className="mb-3">
+        //         <label for="formFileSm" className="form-label">Upload Image</label>
+        //         <input className="form-control form-control-sm" id="formFileSm" type="file" />
         //     </div>
         // </form>
-        <div class="container">
-          <div class="row">
-            <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-              <div class="card border-0 shadow rounded-3 my-4">
-                <div class="card-body p-4 p-sm-5">
-                  <h5 class="card-title text-center mb-5 fw-light fs-5">Create New Person</h5>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+              <div className="card border-0 shadow rounded-3 my-4">
+                <div className="card-body p-4 p-sm-5">
+                  <h5 className="card-title text-center mb-5 fw-light fs-5">New Person Form</h5>
                   <form>
-                    <div class="form-floating mb-3">
-                      <input type="text" class="form-control" id="firstNameInput"/>
-                      <label for="floatingInput">First Name</label>
+                    <div className="form-floating mb-3">
+                      <input type="text" className="form-control" id="firstNameInput" placeholder="First Name"/>
+                      <label for="firstNameInput">First Name</label>
                     </div>
-                    <div class="form-floating mb-3">
-                      <input type="text" class="form-control" id="lastNameInput" />
-                      <label for="floatingInput">Last Name</label>
+                    <div className="form-floating mb-3">
+                      <input type="text" className="form-control" id="lastNameInput" placeholder="Last Name"/>
+                      <label for="lastNameInput">Last Name</label>
                     </div>
-                    <div class="form-floating mb-4">
-                      <input type="text" class="form-control" id="lastNameInput" />
-                      <label for="floatingInput">Last Name</label>
+                    <div className="mb-4">
+                        <label for="imageInput" className="form-label text-start">Upload Image</label>
+                        <input type="file" className="form-control" id="imageInput" />
                     </div>
-                    <div class="d-grid mb-2">
-                      <button class="btn btn-submit btn-login text-uppercase fw-bold" type="submit">
-                        <i class="fab fa-google me-2"></i> Sign in with Google
+                    <div className="d-grid mb-2">
+                      <button className="btn btn-submit btn-login text-uppercase fw-bold" type="submit">
+                        <i className="fab fa-google me-2"></i> Confirm
                       </button>
                     </div>
                   </form>
