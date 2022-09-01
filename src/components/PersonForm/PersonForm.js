2d46e6eb799b0cb09eb83b08e50442c2de410003
Lawrence Dovin <lawrencedovin@gmail.com>
2022-08-31 21:09:40 -0400

    Updated person form input and addPerson function

--- src/components/PersonForm/PersonForm.js
+++ src/components/PersonForm/PersonForm.js
@@ -1,52 +1,80 @@
-import React from "react";
+import React, {useState} from "react";
 import "./PersonForm.css";
 import { client } from "../../utils/axiosClient";
 
 const PersonForm = () => {
 
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [image, setImage] = useState('');
+  const [posts, setPosts] = useState([]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    const addPerson = (firstName, lastName, image) => {
+    addPerson(firstName, lastName, image);
+  };
 
-    }
-  }
+  const addPerson = (firstName, lastName, image) => {
+    client
+       .post('', {
+          firstName,
+          lastName,
+          image
+       })
+       .then((response) => {
+          setPosts([response.data, ...posts]);
+       });
+       setFirstName('');
+       setLastName('');
+       setImage('');
+ };
+
     return (
-        // <form>
-        //     <div className="input-group input-group-sm">
-        //         <span className="input-group-text">First and Last name</span>
-        //         <input type="text" aria-label="First name" className="form-control" />
-        //         <input type="text" aria-label="Last name" className="form-control" />
-        //     </div>
-        //     <div className="mb-3">
-        //         <label for="formFileSm" className="form-label">Upload Image</label>
-        //         <input className="form-control form-control-sm" id="formFileSm" type="file" />
-        //     </div>
-        // </form>
         <div className="container">
           <div className="row">
             <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
               <div className="card border-0 shadow rounded-3 my-4">
                 <div className="card-body p-4 p-sm-5">
                   <h5 className="card-title text-center mb-5 fw-light fs-5">New Person Form</h5>
-                  <formaxi>
+                  <form >
                     <div className="form-floating mb-3">
-                      <input type="text" className="form-control" id="firstNameInput" placeholder="First Name"/>
+                      <input 
+                        type="text" 
+                        className="form-control" 
+                        id="firstNameInput" 
+                        placeholder="First Name"
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                      />
                       <label for="firstNameInput">First Name</label>
                     </div>
                     <div className="form-floating mb-3">
-                      <input type="text" className="form-control" id="lastNameInput" placeholder="Last Name"/>
+                      <input 
+                        type="text" 
+                        className="form-control" 
+                        id="lastNameInput" 
+                        placeholder="Last Name"
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
+                      />
                       <label for="lastNameInput">Last Name</label>
                     </div>
                     <div className="mb-4">
                         <label for="imageInput" className="form-label text-start">Upload Image</label>
-                        <input type="file" className="form-control" id="imageInput" />
+                        <input 
+                          type="file" 
+                          className="form-control" 
+                          id="imageInput" 
+                          value={image}
+                          onChange={(e) => setImage(e.target.value)}
+                        />
                     </div>
                     <div className="d-grid mb-2">
                       <button className="btn btn-submit btn-login text-uppercase fw-bold" type="submit">
                         <i className="fab fa-google me-2"></i> Confirm
                       </button>
                     </div>
-                  </formaxi>
+                  </form>
                 </div>
               </div>
             </div>
